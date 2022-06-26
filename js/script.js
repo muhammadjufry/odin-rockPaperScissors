598cd83056a9877f845d2ab0117b8763c0d4f398
NJ-2020 <ntt.london@gmail.com>
2022-06-26 14:01:24 +0700

    Fix result statements

--- js/script.js
+++ js/script.js
@@ -1,74 +1,74 @@
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors');
-const result = document.querySelector('.result');
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const result = document.querySelector(".result");
 
 function computerPlay() {
-  let randomWeapons = ['rock', 'paper', 'scissors'];
+  let randomWeapons = ["rock", "paper", "scissors"];
   return randomWeapons[Math.floor(Math.random() * randomWeapons.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   let playerSelectionToLower = playerSelection.toLowerCase();
 
   // Player Chooses Rock
-  if (playerSelectionToLower === 'rock') {
-    if (computerSelection === 'rock') {
-      p.innerText = 'Tie!';
+  if (playerSelectionToLower === "rock") {
+    if (computerSelection === "rock") {
+      p.innerText = "Tie!";
       result.appendChild(p);
-    } else if (computerSelection === 'paper') {
-      p.innerText = 'You lose! Paper beats Rock';
+    } else if (computerSelection === "paper") {
+      p.innerText = "You lose! Paper beats Rock";
       result.appendChild(p);
     } else {
-      p.innerText = 'You win! Rock beats Scissors';
+      p.innerText = "You win! Rock beats Scissors";
       result.appendChild(p);
     }
   }
 
   // Player Chooses Paper
-  if (playerSelectionToLower === 'paper') {
-    if (computerSelection === 'rock') {
-      p.innerText = 'You win! Rock beats Paper';
+  if (playerSelectionToLower === "paper") {
+    if (computerSelection === "rock") {
+      p.innerText = "You win! Paper beats Rock";
       result.appendChild(p);
-    } else if (computerSelection === 'paper') {
-      p.innerText = 'Tie!';
+    } else if (computerSelection === "paper") {
+      p.innerText = "Tie!";
       result.appendChild(p);
     } else {
-      p.innerText = 'You lose! Scissors beats Paper';
+      p.innerText = "You lose! Scissors beats Paper";
       result.appendChild(p);
     }
   }
 
   // Player Chooses Scissors
-  if (playerSelectionToLower === 'scissors') {
-    if (computerSelection === 'rock') {
-      p.innerText = 'You lose! Rock beats scissors';
+  if (playerSelectionToLower === "scissors") {
+    if (computerSelection === "rock") {
+      p.innerText = "You lose! Rock beats Scissors";
       result.appendChild(p);
-    } else if (computerSelection === 'paper') {
-      p.innerText = 'You win! Paper beats scissors';
+    } else if (computerSelection === "paper") {
+      p.innerText = "You win! Scissors beats Paper";
       result.appendChild(p);
     } else {
-      p.innerText = 'Tie!';
+      p.innerText = "Tie!";
       result.appendChild(p);
     }
   }
 }
 
-rock.addEventListener('click', function () {
-  const playerSelection = 'rock';
+rock.addEventListener("click", function () {
+  const playerSelection = "rock";
   const computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
 });
 
-paper.addEventListener('click', function () {
-  const playerSelection = 'paper';
+paper.addEventListener("click", function () {
+  const playerSelection = "paper";
   const computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
 });
 
-scissors.addEventListener('click', function () {
-  const playerSelection = 'scissors';
+scissors.addEventListener("click", function () {
+  const playerSelection = "scissors";
   const computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
 });
