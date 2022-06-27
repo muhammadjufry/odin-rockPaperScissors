02333dda8d003f57e9bab0faffdace32dac6a567
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-06-27 12:52:58 +0700

    Simplify the syntax and add a function for the game when it's over

--- js/script.js
+++ js/script.js
@@ -1,116 +1,100 @@
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const result = document.querySelector(".result");
-const playerScoresResult = document.querySelector(".playerScores");
-const computerScoresResult = document.querySelector(".computerScores");
-let playerScores = 0;
-let computerScores = 0;
+let playerScores = 0,
+  computerScores = 0;
+const playerButtons = document.querySelectorAll('.player-button');
+const result = document.querySelector('.result');
+const p = document.createElement('p');
+const playerScoresResult = document.querySelector('.playerScores');
+const computerScoresResult = document.querySelector('.computerScores');
+const h2 = document.createElement('h2');
+const gameOverOutcome = document.querySelector('.gameOver');
 
 function computerPlay() {
-  let randomWeapons = ["rock", "paper", "scissors"];
+  let randomWeapons = ['rock', 'paper', 'scissors'];
   return randomWeapons[Math.floor(Math.random() * randomWeapons.length)];
 }
 
+function getPlayerSelection(event) {
+  const playerSelection = event.target.textContent;
+  const computerSelection = computerPlay();
+  playRound(playerSelection.toLowerCase(), computerSelection);
+  checkForTheWinner(playerScores, computerScores);
+}
+
+playerButtons.forEach((button) =>
+  button.addEventListener('click', getPlayerSelection)
+);
+
 function playRound(playerSelection, computerSelection) {
-  const p = document.createElement("p");
-  let playerSelectionToLower = playerSelection.toLowerCase();
-
   // Player Chooses Rock
-  if (playerSelectionToLower === "rock") {
-    if (computerSelection === "rock") {
-      p.innerText = "Tie!";
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'rock') {
+      p.innerText = 'Tie!';
       result.appendChild(p);
-    } else if (computerSelection === "paper") {
-      p.innerText = "You lose! Paper beats Rock";
+    } else if (computerSelection === 'paper') {
+      p.innerText = 'You lose! Paper beats Rock';
       computerScores++;
       result.appendChild(p);
       computerScoresResult.innerText = 'Computer: ' + computerScores;
     } else {
-      p.innerText = "You win! Rock beats Scissors";
+      p.innerText = 'You win! Rock beats Scissors';
       playerScores++;
       result.appendChild(p);
       playerScoresResult.innerText = 'Player: ' + playerScores;
     }
-  }
-
-  // Player Chooses Paper
-  if (playerSelectionToLower === "paper") {
-    if (computerSelection === "rock") {
-      p.innerText = "You win! Paper beats Rock";
+  } else if (playerSelection === 'paper') {
+    // Player Chooses Paper
+    if (computerSelection === 'rock') {
+      p.innerText = 'You win! Paper beats Rock';
       playerScores++;
       result.appendChild(p);
       playerScoresResult.innerText = 'Player: ' + playerScores;
-    } else if (computerSelection === "paper") {
-      p.innerText = "Tie!";
+    } else if (computerSelection === 'paper') {
+      p.innerText = 'Tie!';
       result.appendChild(p);
     } else {
-      p.innerText = "You lose! Scissors beats Paper";
+      p.innerText = 'You lose! Scissors beats Paper';
       computerScores++;
       result.appendChild(p);
       computerScoresResult.innerText = 'Computer: ' + computerScores;
     }
-  }
-
-  // Player Chooses Scissors
-  if (playerSelectionToLower === "scissors") {
-    if (computerSelection === "rock") {
-      p.innerText = "You lose! Rock beats Scissors";
+  } else if (playerSelection === 'scissors') {
+    // Player Chooses Scissors
+    if (computerSelection === 'rock') {
+      p.innerText = 'You lose! Rock beats Scissors';
       computerScores++;
       result.appendChild(p);
       computerScoresResult.innerText = 'Computer: ' + computerScores;
-    } else if (computerSelection === "paper") {
-      p.innerText = "You win! Scissors beats Paper";
+    } else if (computerSelection === 'paper') {
+      p.innerText = 'You win! Scissors beats Paper';
       playerScores++;
       result.appendChild(p);
       playerScoresResult.innerText = 'Player: ' + playerScores;
     } else {
-      p.innerText = "Tie!";
+      p.innerText = 'Tie!';
       result.appendChild(p);
     }
   }
 }
 
-const checkForTheWinner = function(playerScores, computerScores) {
-  if(playerScores === 5) {
-    const h2 = document.createElement("h2");
+function checkForTheWinner(playerScores, computerScores) {
+  if (playerScores === 5) {
     h2.classList.add('playerWon');
     h2.innerText = `You won ${playerScores} to ${computerScores} great job beating the computer!`;
     result.appendChild(h2);
-  } else if(computerScores === 5) {
-    const h2 = document.createElement("h2");
+    gameOver();
+  } else if (computerScores === 5) {
     h2.classList.add('computerWon');
-    h2.innerText = `You lost ${playerScores} to ${computerScores} great job beating the computer!`;
+    h2.innerText = `You lose ${playerScores} to ${computerScores} great job beating the computer!`;
     result.appendChild(h2);
+    gameOver();
   }
 }
 
-
-rock.addEventListener("click", function () {
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  checkForTheWinner(playerScores, computerScores);
-});
-
-paper.addEventListener("click", function () {
-  const playerSelection = "paper";
-  const computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  checkForTheWinner(playerScores, computerScores);
-});
-
-scissors.addEventListener("click", function () {
-  const playerSelection = "scissors";
-  const computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  checkForTheWinner(playerScores, computerScores);
-});
-
-// function game() {
-//   for (let i = 0; i < 5; i++){
-//     const playerSelection = prompt('Please enter the name of your weapon');
-//     const computerSelection = computerPlay();
-//     console.log(playRound(playerSelection, computerSelection));
-//   }
-// }
+function gameOver() {
+  const h2 = document.createElement('h2');
+  playerButtons.forEach((button) =>
+    button.removeEventListener('click', getPlayerSelection)
+  );
+  h2.innerText = 'The Game is Over!';
+  gameOverOutcome.appendChild(h2);
+}
